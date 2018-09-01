8ed4e043653afbed59bc6ffb64f1749a9a42ae3f
ngoalongtb <ngoalongtb001@gmail.com>
2018-09-01 16:13:20 +0700

    Database and Admin

---
+++ ChatApp/Areas/Admin/Controllers/ChuDesController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ChatApp.EF;
+
+namespace ChatApp.Areas.Admin.Controllers
+{
+    public class ChuDesController : Controller
+    {
+        private ChatDB db = new ChatDB();
+
+        // GET: Admin/ChuDes
+        public ActionResult Index()
+        {
+            var chuDes = db.ChuDes.Include(c => c.DanhMuc);
+            return View(chuDes.ToList());
+        }
+
+        // GET: Admin/ChuDes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ChuDe chuDe = db.ChuDes.Find(id);
+            if (chuDe == null)
+            {
+                return HttpNotFound();
+            }
+            return View(chuDe);
+        }
+
+        // GET: Admin/ChuDes/Create
+        public ActionResult Create()
+        {
+            ViewBag.MaDanhMuc = new SelectList(db.DanhMucs, "MaDanhMuc", "TenDanhMuc");
+            return View();
+        }
+
+        // POST: Admin/ChuDes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MaChuDe,TenChuDe,MaDanhMuc")] ChuDe chuDe)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ChuDes.Add(chuDe);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.MaDanhMuc = new SelectList(db.DanhMucs, "MaDanhMuc", "TenDanhMuc", chuDe.MaDanhMuc);
+            return View(chuDe);
+        }
+
+        // GET: Admin/ChuDes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ChuDe chuDe = db.ChuDes.Find(id);
+            if (chuDe == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.MaDanhMuc = new SelectList(db.DanhMucs, "MaDanhMuc", "TenDanhMuc", chuDe.MaDanhMuc);
+            return View(chuDe);
+        }
+
+        // POST: Admin/ChuDes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MaChuDe,TenChuDe,MaDanhMuc")] ChuDe chuDe)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(chuDe).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.MaDanhMuc = new SelectList(db.DanhMucs, "MaDanhMuc", "TenDanhMuc", chuDe.MaDanhMuc);
+            return View(chuDe);
+        }
+
+        // GET: Admin/ChuDes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ChuDe chuDe = db.ChuDes.Find(id);
+            if (chuDe == null)
+            {
+                return HttpNotFound();
+            }
+            return View(chuDe);
+        }
+
+        // POST: Admin/ChuDes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            ChuDe chuDe = db.ChuDes.Find(id);
+            db.ChuDes.Remove(chuDe);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ChatApp/Areas/Admin/Controllers/ThongTinNhomsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ChatApp.EF;
+
+namespace ChatApp.Areas.Admin.Controllers
+{
+    public class ThongTinNhomsController : Controller
+    {
+        private ChatDB db = new ChatDB();
+
+        // GET: Admin/ThongTinNhoms
+        public ActionResult Index()
+        {
+            var thongTinNhoms = db.ThongTinNhoms.Include(t => t.Nhom);
+            return View(thongTinNhoms.ToList());
+        }
+
+        // GET: Admin/ThongTinNhoms/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ThongTinNhom thongTinNhom = db.ThongTinNhoms.Find(id);
+            if (thongTinNhom == null)
+            {
+                return HttpNotFound();
+            }
+            return View(thongTinNhom);
+        }
+
+        // GET: Admin/ThongTinNhoms/Create
+        public ActionResult Create()
+        {
+            ViewBag.MaNhom = new SelectList(db.Nhoms, "MaNhom", "TenNhom");
+            return View();
+        }
+
+        // POST: Admin/ThongTinNhoms/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MaThongTinNhom,TenTaiKhoan,MaNhom")] ThongTinNhom thongTinNhom)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ThongTinNhoms.Add(thongTinNhom);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.MaNhom = new SelectList(db.Nhoms, "MaNhom", "TenNhom", thongTinNhom.MaNhom);
+            return View(thongTinNhom);
+        }
+
+        // GET: Admin/ThongTinNhoms/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ThongTinNhom thongTinNhom = db.ThongTinNhoms.Find(id);
+            if (thongTinNhom == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.MaNhom = new SelectList(db.Nhoms, "MaNhom", "TenNhom", thongTinNhom.MaNhom);
+            return View(thongTinNhom);
+        }
+
+        // POST: Admin/ThongTinNhoms/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MaThongTinNhom,TenTaiKhoan,MaNhom")] ThongTinNhom thongTinNhom)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(thongTinNhom).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.MaNhom = new SelectList(db.Nhoms, "MaNhom", "TenNhom", thongTinNhom.MaNhom);
+            return View(thongTinNhom);
+        }
+
+        // GET: Admin/ThongTinNhoms/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ThongTinNhom thongTinNhom = db.ThongTinNhoms.Find(id);
+            if (thongTinNhom == null)
+            {
+                return HttpNotFound();
+            }
+            return View(thongTinNhom);
+        }
+
+        // POST: Admin/ThongTinNhoms/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            ThongTinNhom thongTinNhom = db.ThongTinNhoms.Find(id);
+            db.ThongTinNhoms.Remove(thongTinNhom);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}

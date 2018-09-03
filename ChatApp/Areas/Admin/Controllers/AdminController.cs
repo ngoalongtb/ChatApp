9da68d3130b1d307ea07fdb96aa46523665f9b9a
ngoalongtb <ngoalongtb001@gmail.com>
2018-09-03 17:11:44 +0700

    Add fileter and Login Page

---
+++ ChatApp/Areas/Admin/Controllers/AdminController.cs
@@ -0,0 +1,37 @@
+﻿using ChatApp.Areas.Admin.Models;
+using ChatApp.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ChatApp.Areas.Admin.Controllers
+{
+    public class AdminController : Controller
+    {
+        protected override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            TaiKhoan loginAccount = (TaiKhoan)Session["LoginAccount"];
+            if (loginAccount == null)
+            {
+                filterContext.Result = new RedirectResult("~/Admin/Login/Index");
+            }
+
+            if (loginAccount != null && !ValidateRole(loginAccount))
+            {
+                filterContext.Result = new RedirectResult("~/Admin/Dashboard/Index");
+            }
+            //
+            base.OnActionExecuted(filterContext);
+        }
+
+        public bool ValidateRole(TaiKhoan loginAccount)
+        {
+            if (loginAccount.LoaiTaiKhoan.TenLoaiTaiKhoan == Roles.ADMIN)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
---
+++ ChatApp/Areas/Admin/Controllers/LoginController.cs
@@ -0,0 +1,42 @@
+﻿using ChatApp.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ChatApp.Areas.Admin.Controllers
+{
+    public class LoginController : Controller
+    {
+        private ChatDB db = new ChatDB();
+        // GET: Admin/Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string username, string password)
+        {
+            var temp = db.TaiKhoans.Where(x => x.TenTaiKhoan == username && x.MatKhau == password);
+            if (temp.Count() == 1)
+            {
+                TaiKhoan loginAccount = temp.SingleOrDefault();
+                loginAccount.MatKhau = "";
+                Session.Add("LoginAccount", loginAccount);
+                return RedirectToAction("Index", "Dashboard", new { area = "Admin" });
+            }
+            else
+            {
+                TempData["Message"] = "Tài khoản hoặc mật khẩu không chính xác";
+                return RedirectToAction("Index");
+            }
+        }
+        public ActionResult Logout()
+        {
+            Session.Remove("LoginAccount");
+            return Redirect("/");
+        }
+    }
+}
